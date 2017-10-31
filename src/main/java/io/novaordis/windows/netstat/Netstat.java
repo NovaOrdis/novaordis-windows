b89bce49e5f85bab1dd8643e8ea480a8eff530cf
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-10-31 13:00:42 -0700

    added timestamp info and CSV reporting

--- src/main/java/io/novaordis/windows/netstat/Netstat.java
+++ src/main/java/io/novaordis/windows/netstat/Netstat.java
@@ -19,12 +19,16 @@ package io.novaordis.windows.netstat;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Windows netstat output parsing logic.
@@ -58,12 +62,14 @@ public class Netstat {
         STANDARD_PORTS.put("knetd", 2053);
         STANDARD_PORTS.put("man", 9535);
 
-        LOCAL_HOST_ADDRESSES.add("GBDC1-PLMPRD-1");
-        LOCAL_HOST_ADDRESSES.add("10.103.0.130");
-        LOCAL_HOST_ADDRESSES.add("127.0.0.1");
+//        LOCAL_HOST_ADDRESSES.add("GBDC1-PLMPRD-1");
+//        LOCAL_HOST_ADDRESSES.add("10.103.0.130");
+//        LOCAL_HOST_ADDRESSES.add("127.0.0.1");
     }
 
     // Static ----------------------------------------------------------------------------------------------------------
+
+    private static boolean headerDisplayed = false;
 
 
     public static void parse(String[] args) throws Exception {
@@ -77,6 +83,8 @@ public class Netstat {
         String line;
 
         int lineNumber = 0;
+
+        TimestampInfo currentTimestampInfo = null;
 
         Connection current = null;
 
@@ -93,7 +101,31 @@ public class Netstat {
                 continue;
             }
 
-            if (line.startsWith(ConnectionType.TCP.name())) {
+            if (TimestampInfo.isDateLine(line)) {
+
+                //
+                // display the statistics for the previous reading
+                //
+
+                if (currentTimestampInfo != null) {
+
+                    displayStatistics(currentTimestampInfo, connections);
+
+                    //
+                    // reset data and prepare it for the next reading
+                    //
+
+                    connections.clear();
+                }
+
+                currentTimestampInfo = new TimestampInfo(line);
+            }
+            else if (TimestampInfo.isTimeLine(line)) {
+
+                //noinspection ConstantConditions
+                currentTimestampInfo.setTime(line);
+            }
+            else if (line.startsWith(ConnectionType.TCP.name())) {
 
                 if (current != null) {
 
@@ -114,12 +146,16 @@ public class Netstat {
 
         br.close();
 
-        report(connections);
+        if (currentTimestampInfo != null) {
+
+            //
+            // display statistics for the last reading
+            //
+            displayStatistics(currentTimestampInfo, connections);
+        }
     }
 
-    private static void report(List<Connection> connections) {
-
-        int width = 27;
+    private static void displayStatistics(TimestampInfo ti, List<Connection> connections) {
 
         ConnectionState[] states = {
 
@@ -135,26 +171,42 @@ public class Netstat {
                 ConnectionState.SYN_SENT,
         };
 
+        if (!headerDisplayed) {
+
+            headerDisplayed = true;
+
+            System.out.print("# time, ");
+
+            for(ConnectionState s: states) {
+
+                System.out.print(s.name() + " (total), ");
+            }
+
+            for(ConnectionState s: states) {
+
+                System.out.print(s.name() + " (java), ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.print(TimestampInfo.TIMESTAMP_OUTPUT_FORMAT.format(ti.getTimestamp()) + ", ");
+
         for(ConnectionState s: states) {
 
             int c = getCount(connections, s, null);
 
-            if (c == 0) {
-
-                continue;
-            }
-
-            System.out.printf("%-" + width + "s", s.name() + " (total/java):");
-            System.out.print(c);
-            System.out.print("/");
-            c = getCount(connections, s, "java.exe");
-            System.out.println(c);
+            System.out.print(c + ", ");
         }
-    }
 
-    public static boolean isLocalAddress(String s) {
+        for(ConnectionState s: states) {
 
-        return LOCAL_HOST_ADDRESSES.contains(s);
+            int c = getCount(connections, s, "java.exe");
+
+            System.out.print(c + ", ");
+        }
+
+        System.out.println();
     }
 
     /**
@@ -204,5 +256,56 @@ public class Netstat {
     // Private ---------------------------------------------------------------------------------------------------------
 
     // Inner classes ---------------------------------------------------------------------------------------------------
+
+    private static class TimestampInfo {
+
+        public static final Pattern DATE_PATTERN = Pattern.compile("^[1-3][0-9]/[0-1][0-9]/\\d\\d\\d\\d.*");
+        public static final Pattern TIME_PATTERN = Pattern.compile("^[0-2]\\d:\\d\\d.*");
+
+        public static final SimpleDateFormat TIMESTAMP_INPUT_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        public static final SimpleDateFormat TIMESTAMP_OUTPUT_FORMAT = new SimpleDateFormat("MM/dd/YY HH:mm");
+
+        /**
+         * Return true if the line starts with date info.
+         */
+        public static boolean isDateLine(String line) {
+
+            Matcher m = DATE_PATTERN.matcher(line);
+            return m.matches();
+        }
+
+        /**
+         * Return true if the line starts with date info.
+         */
+        public static boolean isTimeLine(String line) {
+
+            Matcher m = TIME_PATTERN.matcher(line);
+            return m.matches();
+        }
+
+        private String dateString;
+        private String timeString;
+        private long timestamp;
+
+        public TimestampInfo(String line) {
+
+            this.dateString = line.trim();
+        }
+
+        public void setTime(String line) throws ParseException {
+
+            this.timeString = line.trim();
+
+            String s = dateString + " " + timeString;
+
+            this.timestamp = TIMESTAMP_INPUT_FORMAT.parse(s).getTime();
+        }
+
+        public long getTimestamp() {
+
+            return timestamp;
+        }
+
+    }
 
 }
